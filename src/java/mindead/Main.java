2dffc55b22635bf6b6df25af62b4fb5a176248f2
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-08-18 14:48:05 +0300

    add Engine and some todo for future

--- src/java/mindead/Main.java
+++ src/java/mindead/Main.java
@@ -1,22 +1,32 @@
 package mindead;
 
+import arc.Events;
 import arc.util.CommandHandler;
+import arc.util.Timer;
+import mindead.types.Engine;
+import mindustry.game.EventType.*;
 import mindustry.mod.Plugin;
 
 public class Main extends Plugin {
 
+    private Engine engine;
+
     @Override
     public void init() {
         Generator.load();
+
+        Timer.schedule(() -> {
+            if (engine != null) engine.update();
+        }, 0f, 0.2f);
+
+        Events.on(PlayerJoin.class, event -> {
+            engine = new Engine(event.player.tileX(), event.player.tileY() + 10);
+        });
     }
 
     @Override
     public void registerClientCommands(CommandHandler handler) {}
 
     @Override
-    public void registerServerCommands(CommandHandler handler) {
-        handler.register("init", "TEST.", args -> {
-            // TODO: some test stuffs
-        });
-    }
+    public void registerServerCommands(CommandHandler handler) {}
 }
---
+++ src/java/mindead/types/Engine.java
@@ -0,0 +1,56 @@
+package mindead.types;
+
+import arc.graphics.Color;
+import arc.math.Mathf;
+import arc.math.geom.Position;
+import arc.util.Interval;
+import mindustry.content.Fx;
+import mindustry.gen.Call;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
+import mindustry.gen.Sounds;
+import mindustry.graphics.Pal;
+
+import static mindustry.Vars.*;
+
+public class Engine implements Position {
+
+    public static float radius = 80f;
+
+    private float x, y;
+    private float progress;
+
+    private Interval timer = new Interval();
+
+    public Engine(int x, int y) { // TODO: add ShortSchematics and spawn engine scheme
+        this.x = x * tilesize + 4f;
+        this.y = y * tilesize + 4f;
+    }
+
+    public void update() {
+        float repairSpeed = 0f;
+        for (Player player : Groups.player)
+            if (within(player, radius)) repairSpeed += 0.01; // TODO: different repair speed for different unit types
+
+        progress = Mathf.clamp(progress + repairSpeed);
+        float visualProgress = repairSpeed == 0f ? 1f : progress;
+
+        Color color = Color.valueOf("38d667").lerp(Pal.health, 1 - visualProgress);
+        for (int deg = 0; deg < visualProgress * 360f; deg += 10)
+            Call.effect(Fx.mineSmall, x + Mathf.cosDeg(deg) * radius, y + Mathf.sinDeg(deg) * radius, 0f, color);
+
+        float volume = progress == 1f ? 1f : .5f;
+        if (timer.get(4.55f * 60f / volume)) // sound length depends on pitch and idk why
+            Call.soundAt(Sounds.combustion, x, y, volume, volume); // volume and pitch are the same
+    }
+
+    @Override
+    public float getX() {
+        return x;
+    }
+
+    @Override
+    public float getY() {
+        return y;
+    }
+}
