8ad1eaf9366a93aa400942b28263d36964a861c9
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-12-11 00:44:02 +0300

    add door generation

--- src/java/mindead/content/Schematics.java
+++ src/java/mindead/content/Schematics.java
@@ -1,17 +1,35 @@
 package mindead.content;
 
+import arc.graphics.Color;
+import mindustry.content.Blocks;
 import mindustry.game.Schematic;
 import mindustry.game.Team;
+import mindustry.world.Tile;
+import mindustry.world.blocks.environment.Prop;
 import useful.ShortSchematics;
 
 import static mindustry.Vars.*;
 
 public class Schematics {
 
-    public static Schematic engine;
+    public static Schematic engine, doorStart, doorFragment, doorEnd;
 
     public static void load() {
+        // region schematics
+
         engine = load(0, -1, "BCCtAAbiaAbbwAabIaabBдABb");
+
+        doorStart = load(-2, -2, "ccBiaABkaABiBABkBABibABkbABiAaBkAaBiaaBkaaBiBaBkBaBibaCtbaBiCaBkCaBiABBkABBiaBBkaBBiBBCtBBBibBBkbBBiCBBkCBBiAbBkAbBiabCtabBiBbBkBbBibbBkbbBiCbBkCbBiACBkACBiCCBkCC");
+        doorFragment = load(-2, -2, "cCBeAA^BBHbABeCABHAaBeaa^aBHba^aBHAB^aBeBB^aBHCBBeAbBHabBeBb^B");
+        doorEnd = load(-2, -2, "ccBiAABkAABiCABkCABiAaBkAaBiaaBkaaBiBaBkBaBibaCtbaBiCaBkCaBiABBkABBiaBBkaBBiBBCtBBBibBBkbBBiCBBkCBBiAbBkAbBiabCtabBiBbBkBbBibbBkbbBiCbBkCbBiaCBkaCBiBCBkBCBibCBkbC");
+
+        // endregion
+        // region config
+
+        doorStart.tiles.each(st -> st.block == Blocks.illuminator, st -> st.config = Color.scarlet.rgba());
+        doorEnd.tiles.each(st -> st.block == Blocks.illuminator, st -> st.config = Color.scarlet.rgba());
+
+        // endregion
     }
 
     public static Schematic load(int x, int y, String base) {
@@ -25,5 +43,40 @@ public class Schematics {
 
     public static void at(Schematic schematic, int x, int y, Team team) {
         schematic.tiles.each(st -> world.tile(st.x + x, st.y + y).setBlock(st.block, team, st.rotation));
+    }
+
+    public static void terrainAt(Schematic schematic, int x, int y) {
+        schematic.tiles.each(st -> {
+            Tile tile = world.tile(st.x + x, st.y + y);
+
+            if (st.block.isFloor() && !st.block.isOverlay())
+                tile.setFloorUnder(st.block.asFloor());
+            else if (st.block instanceof Prop)
+                tile.setBlock(st.block);
+            else if (st.block.isOverlay())
+                tile.setOverlay(st.block);
+            else
+                tile.setBlock(st.block, Team.malis);
+
+            if (st.config != null) tile.build.configure(st.config);
+        });
+    }
+
+    public static void terrainAt(Schematic schematic, int x, int y, boolean vertical) {
+        if (vertical)
+            terrainAt(schematic, x, y);
+        else {
+            flip(schematic);
+            terrainAt(schematic, x, y);
+            flip(schematic);
+        }
+    }
+
+    private static void flip(Schematic schematic) {
+        schematic.tiles.each(st -> {
+            short temp = st.x;
+            st.x = st.y;
+            st.y = temp;
+        });
     }
 }
--- src/java/mindead/types/Door.java
+++ src/java/mindead/types/Door.java
@@ -1,11 +1,36 @@
 package mindead.types;
 
+import arc.func.Cons2;
+import arc.math.Mathf;
+import mindead.content.Schematics;
+import mindustry.game.Schematic;
 import mindustry.world.Tile;
 
 public class Door {
 
+    public int sx, sy, ex, ey, dx, dy;
+
     public Door(Tile start, Tile end) {
-        start.setAir(); // TODO temp
-        end.setAir();
+        this.sx = start.x < end.x ? start.x : end.x;
+        this.ex = start.x < end.x ? end.x : start.x;
+
+        this.sy = start.y < end.y ? start.y : end.y;
+        this.ey = start.y < end.y ? end.y : start.y;
+
+        this.dx = Mathf.clamp(ex - sy, 0, 1);
+        this.dy = Mathf.clamp(ey - sy, 0, 1);
+
+        if (sx == ex) generate(sy, ey, (schema, y) -> Schematics.terrainAt(schema, sx, y, true)); // vertical
+        if (sy == ey) generate(sx, ex, (schema, x) -> Schematics.terrainAt(schema, x, sy, false)); // horizontal
     }
+
+    public void generate(int from, int to, Cons2<Schematic, Integer> cons) {
+        for (int i = from + 4; i < to - 2; i += 4)
+            cons.get(Schematics.doorFragment, i);
+
+        cons.get(Schematics.doorStart, from);
+        cons.get(Schematics.doorEnd, to);
+    }
+
+    public void update() {} // TODO win for survivors and lose for murderer
 }
